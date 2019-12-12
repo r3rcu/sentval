e3917e05905700b56ffcad6027906da87d6750d5
Ridosbey Milian <ridosbeymi@gmail.com>
2019-12-12 14:11:21 -0500

    subiendo el tema de sentval commit inicial

---
+++ sentval_theme/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sentval Black Theme",
+    'version': '1.0',
+    'summary': 'Sentval Bootstrap',
+    'description': """
+Sentval Bootstrap.
+====================
+    """,
+    'author': "sentval.com",
+    'website': "http://www.sentval.com",
+    'category': 'Theme',
+    'depends': ['website_crm'],
+    'demo': [
+        'data/homepage_data.xml',
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+    'images': ['static/description/main_image.png'],
+    'license': 'OPL-1',
+}
